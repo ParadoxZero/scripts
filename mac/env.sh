f783dfd017831318dc75eb6d7ebd0725430e845e
Sidhin S Thomas <thomas.sidhin@outlook.com>
2023-02-14 22:36:23 +0530

    added mac alias

---
+++ mac/env.sh
@@ -0,0 +1,22 @@
+# shorcuts
+alias ..='cd ..'
+alias ....='cd ../..'
+
+# Git alias
+alias status='git status'
+alias branch='git branch'
+alias checkout='git checkout'
+alias commit='git commit -m'
+alias ga='git add'
+
+
+# LSD alias
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias gs='git status'
+
+#config
+alias config_env='code ~/scripts/mac/env.sh'
